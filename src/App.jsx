fe16fec10a52f1b8e9a2ae8efcac96388c5989a7
obafisayo <abimbolaobafisayo@gmail.com>
2024-06-16 00:11:29 +0100

    created new repository for Onekey bags

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import ScrollToTop from './components/ScrollToTop/ScrollToTop';
+import Router from './routes/Routes';
+
+
+function App() {
+  return (
+    <>
+      <Router />
+      <ScrollToTop />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/LandingPage/home/Home.jsx
@@ -0,0 +1,12 @@
+import Container from "../../../components/container/Container";
+import HeroSection from "../../../components/heroSection/HeroSection";
+
+function Home() {
+    return (
+        <Container className="container">
+            <HeroSection />
+        </Container>
+    );
+}
+
+export default Home;
